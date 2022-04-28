3568f2d21452f90a24986274787e212dd560b1b9
tarikaudi <tarik.audi@hotmail.com>
2022-04-28 16:16:55 -0700

    calcu

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,11 +1,9 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    import sys
-    if((len(sys.argv) - 1) == 0):
-        print(f"{len(sys.argv) - 1} arguments.")
-    elif((len(sys.argv) - 1 == 1)):
-        print(f"{len(sys.argv) - 1} argument:")
-    else:
-        print(f"{len(sys.argv) - 1} arguments:")
-    for argument in range(1, len(sys.argv)):
-        print(f"{argument}: {sys.argv[argument]}")
+    from calculator_1 import add, sub, mul, div
+    a = 10
+    b = 5
+    print(f"{a} + {b} = {add(a, b)}")
+    print(f"{a} - {b} = {sub(a, b)}")
+    print(f"{a} * {b} = {mul(a, b)}")
+    print(f"{a} / {b} = {div(a, b)}")
