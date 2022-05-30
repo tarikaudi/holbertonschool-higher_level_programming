bd6b3c75e9deba15e0bc448400eabf92b4d85efe
tarikaudi <tarik.audi@hotmail.com>
2022-05-30 14:34:43 -0700

    issame

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""function tht returns true or false"""
+
+
+def is_same_class(obj, a_class):
+    """comment to func"""
+    if type(obj) is a_class:
+        return True
+    else:
+        return False
