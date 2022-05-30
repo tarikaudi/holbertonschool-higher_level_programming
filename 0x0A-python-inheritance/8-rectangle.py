3ac6d7738960e64dd61626834c057a3ed5edc7ed
tarikaudi <tarik.audi@hotmail.com>
2022-05-30 15:12:03 -0700

    rec

---
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""odule to func dor the checker"""
+
+
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+"""imported func"""
+
+
+class Rectangle(BaseGeometry):
+    """Rectangle class"""
+
+
+    def __init__(self, width, height):
+        super().integer_validator("width", width)
+        super().integer_validator("height", height)
+        self.__width = width
+        self.__height = height
