d15f94161577ab6323fe575d9241d81690a03ddd
tarikaudi <tarik.audi@hotmail.com>
2022-05-30 15:50:52 -0700

    mylist func

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""ceate create for the checker"""
+
+class MyList(list):
+    """class"""
+
+    def print_sorted(self):
+        """print the list"""
+        print(sorted(self))
