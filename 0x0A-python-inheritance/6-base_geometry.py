860c779e3c18da4727013fa2af992abc39a68cbf
tarikaudi <tarik.audi@hotmail.com>
2022-05-30 14:52:27 -0700

    6base

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""this module module func"""
+
+
+class BaseGeometry():
+    """class of base geometry"""
+
+    def area(self):
+        """comment for the checker"""
+        raise Exception("area() is not implemented")
