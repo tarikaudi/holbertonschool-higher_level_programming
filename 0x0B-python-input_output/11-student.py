36da67e54b3e6d007fa58281d7caced81e04f497
tarikaudi <tarik.audi@hotmail.com>
2022-05-31 15:18:12 -0700

    stud

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""module func for the"""
+
+
+class Student():
+    """class student"""
+
+    def __init__(self, first_name, last_name, age):
+        """age first last name"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """return json"""
+        if attrs is None:
+            return self.__dict__
+        else:
+            new = {}
+            for attribute in attrs:
+                if attribute in self.__dict__.keys():
+                    new[attribute] = self.__dict__[attribute]
+            return new
+
+    def reload_from_json(self, json):
+        """replace stricomments"""
+        for i in json:
+            if i in self.__dict__:
+                self.__dict__[i] = json[i]
