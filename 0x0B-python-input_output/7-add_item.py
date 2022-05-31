56914f45bdf54c796a59bf13ea2fd5874342ce41
tarikaudi <tarik.audi@hotmail.com>
2022-05-31 14:44:16 -0700

    fixed

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -11,7 +11,7 @@ save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 add = "add_item.json"
 
 try:
-    loaded = load_from_json_file(fname)
+    loaded = load_from_json_file(add)
 except FileNotFoundError:
     loaded = []
 
