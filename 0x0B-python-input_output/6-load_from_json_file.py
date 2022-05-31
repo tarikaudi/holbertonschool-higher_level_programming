94e883bff368d81b97436646b49cb13de3cf6141
tarikaudi <tarik.audi@hotmail.com>
2022-05-31 15:01:32 -0700

    fixed?

--- 0x0B-python-input_output/6-load_from_json_file.py
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -7,6 +7,6 @@ import json
 
 def load_from_json_file(my_obj, filename):
     """write overrite"""
-    with open(filename, 'r', encoding="UTF8") as data:
+    with open(filename, encoding="UTF8") as data:
         file_loaded = json.load(data)
     return file_loaded
