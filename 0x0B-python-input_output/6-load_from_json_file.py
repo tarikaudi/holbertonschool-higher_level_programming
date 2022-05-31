9849c0cbf6d12fd10d41d51c09f1bc1d37759e8c
tarikaudi <tarik.audi@hotmail.com>
2022-05-31 14:22:49 -0700

    loaded

---
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""comments are iḿportants"""
+
+
+import json
+
+
+def save_to_json_file(my_obj, filename):
+    """write overrite"""
+    with open(filename, 'r', encoding="UTF8") as data:
+        file_loaded = json.load(f)
+    return file_loaded
