66c1c2a3ffdd14a76407d2a0e28ea2981ccea7da
tarikaudi <tarik.audi@hotmail.com>
2022-07-05 13:10:56 -0700

    fix

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -1,4 +1,4 @@
---cerate a full table
+-- cerate a full table
 CREATE TABLE IF NOT EXISTS second_table(id INT, name VARCHAR(256), score INT);
 INSERT INTO second_table VALUES(1, "John", 10);
 INSERT INTO second_table VALUES(2, "Alex", 3);
