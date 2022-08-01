0f29775cf69c6c19b62ec079997bf77eb840265d
root <tarik.audi@hotmail.com>
2022-08-01 12:29:31 -0700

    ,iss

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -5,7 +5,7 @@ const a = process.argv[2];
 if (a === undefined || isNaN(parseInt(a))) {
   console.log('Missing number of occurrences');
 } else {
-  for (let i = 0; i < x; i++) {
+  for (let i = 0; i < a; i++) {
     console.log('C is fun');
   }
 }
