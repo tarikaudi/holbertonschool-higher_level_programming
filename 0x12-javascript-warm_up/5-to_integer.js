09c5a8586273ae08da7b1d958c4cadae3e6cf3a4
root <tarik.audi@hotmail.com>
2022-08-01 11:39:46 -0700

    fix

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
 
-const myVar = process.argv[2]
+const myVar = process.argv[2];
 
 if (isNaN(myVar)) {
   console.log(myVar);
 } else {
-  console.log('My number: ' + parseInt(myVar))
+  console.log('My number: ' + parseInt(myVar));
 }
