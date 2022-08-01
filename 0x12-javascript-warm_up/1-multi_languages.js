95319751621fd75ac596cb8402829d17517d8dc7
root <tarik.audi@hotmail.com>
2022-08-01 11:13:16 -0700

    js

---
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+
+const var1 = 'C is fun';
+const var2 = 'Python is cool';
+const var3 = 'JavaScript is amazing';
+console.log(var1);
+console.log(var2);
+console.log(var3);
---
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+
+if (2 in process.argv) {
+	console.log(process.argv[2]);
+} else {
+	console.log('No argument');
+}
