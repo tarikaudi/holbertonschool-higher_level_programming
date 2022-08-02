880de51f1d3f555a3b5f5c6b285bc79cdf53b274
root <tarik.audi@hotmail.com>
2022-08-02 09:08:25 -0700

    rec

--- 0x13-javascript_objects_scopes_closures/1-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/1-rectangle.js
@@ -2,9 +2,7 @@
 
 module.exports = class Rectangle {
   constructor (w, h) {
-    if (w > 0 && h > 0) {
-      this.width = w;
-      this.height = h;
-    }
+    this.width = w;
+    this.height = h;
   }
 };
