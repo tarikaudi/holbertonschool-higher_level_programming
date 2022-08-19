806a0f8bef64c79ee2335c34000121e88c1d3464
root <tarik.audi@hotmail.com>
2022-08-19 21:47:25 +0000

    hbt

---
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""comment for the checker"""
+
+import requests
+from sys import argv
+
+if __name__ == "__main__":
+    response = requests.get(argv[1])
+    print(response.headers.get('X-Request-Id'))
