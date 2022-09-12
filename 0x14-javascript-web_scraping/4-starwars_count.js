fe068c7e239d3cedcedf41d152c437ba6c759ea3
root <tarik.audi@hotmail.com>
2022-09-12 20:05:06 +0000

    star

---
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,23 @@
+#!/usr/bin/node
+/* comment for the checker */
+
+const axios = require('axios').default;
+
+const url = process.argv[2];
+
+axios.get(url)
+  .then((res) => {
+    const movies = res.data.results;
+    let count = 0;
+
+    for (const arr of movies) {
+      const perso = arr.characters;
+      for (const personaje of perso) {
+        if (personaje.includes('18')) {count++; }
+      }
+    }
+    console.log(count);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
